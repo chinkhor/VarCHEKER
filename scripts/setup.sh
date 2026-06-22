f2211b80bb811ae50b77aa80bf83b0aee092ea43
chinkhor <chin.kooi.khor@gmail.com>
2026-06-22 10:58:22 -0500

    Add pandas installation

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,6 +2,7 @@
 # install dependencies
 sudo apt-get update
 sudo apt-get install -y python3 python3-pip
-sudo apt-get install python3-z3
-sudo apt-get install python3-sympy
+sudo apt-get install -y python3-z3
+sudo apt-get install -y python3-sympy
+sudo apt-get install -y python3-pandas
 
