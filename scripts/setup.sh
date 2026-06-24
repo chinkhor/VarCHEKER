c8e04c0deddaa513f5273eb186957e354dc134a3
chinkhor <chin.kooi.khor@gmail.com>
2026-06-23 21:47:50 -0500

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # install dependencies
-sudo apt-get update
+#sudo apt-get update
 sudo apt-get install -y python3 python3-pip python3-venv
 python3 -m venv venv
 source venv/bin/activate
